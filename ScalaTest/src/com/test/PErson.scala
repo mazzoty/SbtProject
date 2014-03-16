3889d5f71fd71f9dc2544044a6e9a8413cbc0bce
mazzoty <mazzoty@gmail.com>
2014-03-16 12:14:02 +0200

    new comit change of directions from anonymous function

--- ScalaTest/src/com/test/PErson.scala
+++ ScalaTest/src/com/test/PErson.scala
@@ -3,6 +3,14 @@ package com.test
 /**
  * Created by silviu on 3/16/14.
  */
-class Person {
+case class Person (name:String,surname:String){
+
+
+  def getFullName(name:String,surname:String):String={
+   name + "/"+surname
+  }
+
+  def getAllNames(name:String) = (surname:String) =>  name+surname
+
 
 }
